3fc6088bd3ab149ce7645968dcd2f8ca33a141e9
Dmitry Odnorog <Vibiy@yandex.ru>
2018-09-19 12:09:10 +0300

    new model Tag, new fields in Task model

--- todo/admin.py
+++ todo/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Task, TaskGroup
+from .models import Task, TaskGroup, Tag
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['name', 'group']
+    list_display = ['name', 'group', 'created', 'modified']
 
 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(TaskGroup)
+admin.site.register(Tag)
--- todo/models.py
+++ todo/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 
 
-class Task(models.Model):
-    name = models.CharField(max_length=30)
-    group = models.ForeignKey('todo.TaskGroup', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
 class TaskGroup(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=30)
+    group = models.ForeignKey('todo.TaskGroup', on_delete=models.CASCADE)
+    comment = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('todo.Tag')
+
+    def __str__(self):
+        return self.name
+
