9dd52b3247a53802118bafe3beec6e542d0acec2
Dmitry Odnorog <Vibiy@yandex.ru>
2018-09-20 11:03:37 +0300

    all works, but many sql queries

--- todo/views.py
+++ todo/views.py
@@ -1,33 +1,27 @@
-from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.urls import reverse, reverse_lazy
 
 from .models import Task, TaskGroup
 from .forms import TaskModelForm, TaskGroupModelForm
 
 
-class TaskGroupListView(ListView):
+class TaskGroupList(ListView):
     model = TaskGroup
-    context_object_name = 'task_group_list'
     template_name = 'index.html'
 
 
-class TaskListView(ListView):
-    model = Task
-    context_object_name = 'task_list'
+class TaskGroupItem(DetailView):
+    model = TaskGroup
     template_name = 'detail.html'
 
     def get_queryset(self):
-        context = Task.objects.filter(group=self.kwargs['pk'])
-        return context
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = TaskGroup.objects.get(id=self.kwargs['pk']).name
-        return context
+        qs = TaskGroup.objects.filter(id=self.kwargs['pk'])
+        return qs
 
 
-class TaskGroupCreateView(CreateView):
+class TaskGroupCreate(CreateView):
     model = TaskGroup
     form_class = TaskGroupModelForm
     context_object_name = 'form'
@@ -35,11 +29,11 @@ class TaskGroupCreateView(CreateView):
     success_url = reverse_lazy('index')
 
 
-class TaskCreateView(CreateView):
+class TaskCreate(CreateView):
     model = Task
     form_class = TaskModelForm
     context_object_name = 'form'
     template_name = 'task_add.html'
 
     def get_success_url(self):
-        return reverse('detail', kwargs={'pk': self.object.id})
+        return reverse('detail', kwargs={'pk': self.object.group.id})
