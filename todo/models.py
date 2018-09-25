c5510d3435090c217e05004b35a065fd203d2dab
Dmitry Odnorog <Vibiy@yandex.ru>
2018-09-25 13:32:45 +0300

    add new manager for model Task (new method "active()" - filter active tasks)

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class ActiveQuerySet(models.QuerySet):
+
+    def active(self):
+        return self.filter(active=True)
+
+
+class ActiveManager(models.Manager):
+
+    def get_queryset(self):
+        return ActiveQuerySet(self.model, using=self._db)
+
+    def active(self):
+        return self.get_queryset().active()
+
+
 class TaskGroup(models.Model):
     name = models.CharField(max_length=30, unique=True)
 
@@ -23,6 +38,7 @@ class Task(models.Model):
     modified = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField('todo.Tag', blank=True)
     active = models.BooleanField(default=False)
+    objects = ActiveManager()
 
     def __str__(self):
         return self.name
