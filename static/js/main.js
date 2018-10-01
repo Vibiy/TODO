c2d81de59151193d29957f6c12068c4801a308e0
Dmitry Odnorog <Vibiy@yandex.ru>
2018-10-01 14:39:33 +0300

    ready index page

--- static/js/main.js
+++ static/js/main.js
@@ -16,32 +16,33 @@ var csrftoken = getCookie('csrftoken');
 
 
 $(document).ready(function(){
-                var $myForm = $('.my-ajax-form')
-                $myForm.submit(function(event){
-                    event.preventDefault()
-                    var $formData = $(this).serialize()
-                    var $thisURL = $myForm.attr('data-url') || window.location.href
-                    $.ajax({
-                        method: "POST",
-                        url: $thisURL,
-                        data: $formData,
-                        success: handleFormSuccess,
-                        error: handleFormError,
-                    })
+    var $myForm = $('.my-ajax-form')
+    $myForm.submit(function(event){
+        event.preventDefault()
+        var $formData = $(this).serialize()
+        var $thisURL = $myForm.attr('data-url') || window.location.href
+        $.ajax({
+            method: 'POST',
+            url: $thisURL,
+            data: $formData,
+            success: handleFormSuccess,
+            error: handleFormError,
+        })
 
-                    function handleFormSuccess(data, textStatus, jqXHR){
-                        console.log(data)
-                        console.log(textStatus)
-                        console.log(jqXHR)
-                        $myForm[0].reset(); // reset form data
-                        alert(data.message)
-                    }
+        function handleFormSuccess(data, textStatus, jqXHR){
+            console.log(data)
+            console.log(textStatus)
+            console.log(jqXHR)
+            $myForm[0].reset(); // reset form data
+            alert(data.message)
+            $('#ajax-list').load($thisURL + " #ajax-list")
+        }
 
-                    function handleFormError(jqXHR, textStatus, errorThrown){
-                        console.log(jqXHR)
-                        console.log(textStatus)
-                        console.log(errorThrown)
-                        alert(data.message)
-                    }
-                })
+        function handleFormError(jqXHR, textStatus, errorThrown){
+            console.log(jqXHR)
+            console.log(textStatus)
+            console.log(errorThrown)
+            alert(data.message)
+        }
+    })
 })
